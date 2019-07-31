7b53275e633a5132b2a1428c024f37aee74f73e9
Awasthi <dawasthi@3inmdawasthi2.ad.here.com>
2019-07-31 19:26:25 +0530

    initial commit

--- fylehome/models.py
+++ fylehome/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Bank(models.Model):
+    ifsc = models.CharField(max_length=30)
+    bank_id = models.CharField(max_length=30)
+    branch = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    bank_name = models.CharField(max_length=50)
+
+
